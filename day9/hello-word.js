9b5f0275ab2f01d422c8ff09c1a646a89de1b969
dinhcongtrinh <117736855+dinhcongtrinh@users.noreply.github.com>
2022-11-26 18:02:58 +0700

    post

---
+++ day9/hello-word.js
@@ -0,0 +1,13 @@
+//unary - toan tu down
+    // khong bawt dau bang so 
+    // quy uoc ; su dung format camelcase
+    // su dung ten co y nghia 
+    // cac bien maf hua gia tri lo gic ne bawt ddu babg 
+    is  . has ...
+    // isvali
+    function greeting() {
+        console.log("hello trinh");
+        console.log("well com to hp");
+    }
+
+    greeting("trinh"); //  name= trrinh
